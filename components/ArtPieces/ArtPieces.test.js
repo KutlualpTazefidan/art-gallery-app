057b40b088a1953af604b8f0d7bc33973d934be4
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-04-26 15:39:39 +0200

    User story 1: add one more test

---
+++ components/ArtPieces/ArtPieces.test.js
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react";
+import { within } from "@testing-library/dom";
+import ArtPieces from ".";
+
+const pieces = [
+  {
+    name: "My Title",
+    imageSource: "http://example.org/my-title.jpg",
+    artist: "Jane Doe",
+    slug: "my-title",
+  },
+  {
+    name: "Apples and Oranges",
+    imageSource: "http://example.org/apples-and-oranges.jpg",
+    artist: "Picasso",
+    slug: "apples-and-oranges",
+  },
+];
+
+test("list of pieces is displayed", () => {
+  render(<ArtPieces pieces={pieces} />);
+
+  const items = screen.getAllByRole("listitem");
+
+  expect(items.length).toBe(2);
+
+  const firstItem = items[0];
+  expect(within(firstItem).getByText("My Title")).toBeInTheDocument();
+
+  const secondItem = items[1];
+  expect(
+    within(secondItem).getByText("Apples and Oranges")
+  ).toBeInTheDocument();
+});
