306a543518b42382ae1cb5fc1fa85928ba01a8e9
= <=>
2023-04-26 16:23:07 +0200

    User Story 2: Add function  chooseRandomPiece

--- pages/spotlight.js
+++ pages/spotlight.js
@@ -1,5 +1,19 @@
+import useSWR from "swr";
 import Spotlight from "../components/Spotlight";
 
+const URL = "https://example-apis.vercel.app/api/art";
+
 export default function SpotlightPage() {
-  return <Spotlight />;
+  const { data } = useSWR(URL);
+
+  console.log("data: ", data);
+
+  if (!data) return <div>Loading...</div>;
+
+  function chooseRandomPiece(pieces) {
+    const randomNumber = Math.floor(Math.random() * pieces.length);
+    return pieces[randomNumber];
+  }
+
+  return <Spotlight image={"a"} artist={"s"} />;
 }
