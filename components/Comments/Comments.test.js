cbba46e8198e0d78c8ae1977afaf431b0b7fd7d9
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-04-28 12:29:09 +0200

    displays each comment's date and time

--- components/Comments/Comments.test.js
+++ components/Comments/Comments.test.js
@@ -3,8 +3,8 @@ import Comments from "../Comments";
 
 describe("Comments", () => {
   const comments = [
-    { id: 1, text: "my comment1" },
-    { id: 2, text: "my comment2" },
+    { id: 1, text: "my comment1", createdAt: new Date("2023-04-28T10:15:00Z") },
+    { id: 2, text: "my comment2", createdAt: new Date("2023-01-01T08:05:00Z") },
   ];
 
   it("displays each comment's text", () => {
@@ -12,5 +12,12 @@ describe("Comments", () => {
 
     expect(screen.getByText("my comment1")).toBeInTheDocument();
     expect(screen.getByText("my comment2")).toBeInTheDocument();
+  });
+
+  it("displays each comment's date and time", () => {
+    render(<Comments comments={comments} />);
+
+    expect(screen.getByText("Fri Apr 28 2023 12:15")).toBeInTheDocument();
+    expect(screen.getByText("Sun Jan 01 2023 09:05")).toBeInTheDocument();
   });
 });
--- components/Comments/index.js
+++ components/Comments/index.js
@@ -2,7 +2,14 @@ export default function Comments({ comments = [] }) {
   return (
     <ul>
       {comments.map((comment) => {
-        return <li key={comment.id}>{comment.text}</li>;
+        return (
+          <li key={comment.id}>
+            <div>
+              <div>{comment.text}</div>
+              <div>{comment.createdAt.toString().substring(0, 21)}</div>
+            </div>
+          </li>
+        );
       })}
     </ul>
   );
