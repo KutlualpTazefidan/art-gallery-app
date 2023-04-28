2ce351566ed756ec008b35b161a252837bdeb7c1
= <=>
2023-04-28 10:39:49 +0200

    User Story 10: Add global var for colors

--- styles.js
+++ styles.js
@@ -10,5 +10,10 @@ export default createGlobalStyle`
   body {
     margin: 0;
     font-family: system-ui;
+    --primary-color: #222222;
+    --secondary-color: #F9DBBB;
+    --third-color:rgb(232, 249, 253, 0.7);
+    --forth-color: rgb(252, 41, 71,0.7);
+    --text-color: white;
   }
 `;
